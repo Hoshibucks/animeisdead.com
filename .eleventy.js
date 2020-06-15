b92f290b57d73104dde9863e85543e1d50fd5bd8
Henry Trang <mail@henrytrang.com>
2020-06-14 19:06:37 -0700

    v1.0.1

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
   });
+
+  eleventyConfig.addPassthroughCopy("src/assets");
   
   return {
     // Sets the input and output
